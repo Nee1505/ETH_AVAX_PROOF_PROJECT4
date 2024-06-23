010e4403fe5306b60387b45d737ae5413b91cbba
Nee1505 <meritneelam2004@gmail.com>
2024-06-23 09:46:04 +0530

    Update Project_4.sol

--- contracts/Project_4.sol
+++ contracts/Project_4.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-// Ownable.sol from OpenZeppelin
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/access/Ownable.sol";
